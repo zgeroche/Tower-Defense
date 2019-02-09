46b7f2c398760d1b2c368a0f43d444f7a25d2c28
dpkyad <34806509+dpkyad@users.noreply.github.com>
2019-02-08 18:03:28 -0800

    changed goldenarmor to solider for testing purposes. rearranged GameScene code, seperated out code into functions

--- js/scenes/LoadScene.js
+++ js/scenes/LoadScene.js
@@ -8,19 +8,19 @@ export class LoadScene extends Phaser.Scene{
     }
 
     preload(){
-        this.load.atlas('deathknight', './assets/deathknight.png', 'assets/deathknight.json');
-        this.load.atlas('goldenarmor', './assets/goldenarmor.png', 'assets/goldenarmor.json');
-        this.load.atlas('peasant', './assets/peasant.png', 'assets/peasant.json');
-        this.load.spritesheet('bard', './assets/bard.png', { frameWidth: 52, frameHeight: 75});
-        this.load.image('attack', './assets/coin.png');
-        this.load.image('map', './assets/castle-gates.png', { frameWidth: 640, frameHeight: 512 });
-        this.load.audio('dkDeath', './assets/Sounds/Death Screams/Human/sfx_deathscream_human5.wav');
-        this.load.audio('hit', './assets/Sounds/General Sounds/Simple Damage Sounds/sfx_damage_hit2.wav');
-        this.load.audio('walk', './assets/Sounds/Movement/Footsteps/sfx_movement_footstepsloop4_slow.wav');
-        this.load.audio('background', './assets/Sounds/random silly chip song.ogg');
-
-        this.load.image('menuscreen', './assets/bg.png', { frameWidth: 640, frameHeight: 512 });
-        this.load.image('newgame', './assets/new.png');
+        this.load.atlas('deathknight', './dist/assets/deathknight.png', './dist/assets/deathknight.json');
+        this.load.atlas('solider', './assets/solider.png', './dist/assets/solider.json');
+        this.load.atlas('peasant', './dist/assets/peasant.png', './dist/assets/peasant.json');
+        this.load.spritesheet('bard', './dist/assets/bard.png', { frameWidth: 52, frameHeight: 75});
+        this.load.image('attack', './dist/assets/coin.png');
+        this.load.image('map', './dist/assets/castle-gates.png', { frameWidth: 640, frameHeight: 512 });
+        this.load.audio('dkDeath', './dist/assets/Sounds/Death Screams/Human/sfx_deathscream_human5.wav');
+        this.load.audio('hit', './dist/assets/Sounds/General Sounds/Simple Damage Sounds/sfx_damage_hit2.wav');
+        this.load.audio('walk', './dist/assets/Sounds/Movement/Footsteps/sfx_movement_footstepsloop4_slow.wav');
+        this.load.audio('background', './dist/assets/Sounds/random silly chip song.ogg');
+		this.load.audio('upgradeSound', './dist/dist/assets/Sounds/General Sounds/Positive Sounds/sfx_sounds_powerup6.wav');
+        this.load.image('menuscreen', './dist/assets/bg.png', { frameWidth: 640, frameHeight: 512 });
+        this.load.image('newgame', './dist/assets/new.png');
     }
     create(){
         //Creating menu screen background layers
