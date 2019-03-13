4fda4d1ac3adcfa4053fd857649e883196d6ee97
dpkyad <34806509+dpkyad@users.noreply.github.com>
2019-03-12 22:03:23 -0700

    Fixed a bug where wave detail was not resetting when replaying first level

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -16,7 +16,7 @@ export class GameScene extends Phaser.Scene {
 		GV.scene = CST.SCENES.GAME;
 		GV.PLAYER_HEALTH = 100;
         GV.WAVE = 1;
-        GV.GOLD = 30;
+        GV.GOLD = 50000;
         GV.SPAWNED = 0;
 		
 		GV.WAVE_DETAIL = ['10 Skeletons', '10 Witches', '10 Goblins (fast)', '10 bats (flying)', '10 Skeletons + Boss', '20 Goblins (mass, fast)', '10 witches + 10 bats (flying)', '25 Skeletons (mass)' , '10 Skeletons, Witches & Goblins', '2 Ogre Boss'];
